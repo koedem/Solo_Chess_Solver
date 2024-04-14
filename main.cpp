f8432ccee7165dee3cf2153ca29a63b61443542f
Kolja Kühn <kolja@heck-kuehn.de>
2024-04-14 14:33:32 +0200

    Fix move printing.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 int main() {
     int size = 2;
-    std::vector<std::vector<Piece >> example = { { 2, 0 }, { 0, 1 } };
+    std::vector<std::vector<Piece >> example = { { ONE, NON }, { TWO, NIL } };
     Position position(example);
     Board board(position, size);
 
