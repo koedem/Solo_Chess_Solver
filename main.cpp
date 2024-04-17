cc9f0bc89f493f32ff468412a94ef548b5ce4379
Kolja Kühn <kolja@heck-kuehn.de>
2024-04-17 12:50:21 +0200

    Actually commit the correct file.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "Board.h"
 #include "DFS.h"
+#include "Positions.h"
 
 void solve(const Board& board) {
     auto moves = board.generate_moves();
@@ -14,79 +15,25 @@ void solve(const Board& board) {
 }
 
 int main() {
-    int size = 10;
-    std::vector<std::vector<Piece >> old_variable_assigment = {
-            { NON, NON, NON, NON, NON, NON, NON, TWO, NON, NIL },
-            { NON, NON, NON, NON, NON, NON, NON, TWO, NON, NIL },
-            { NON, NON, NON, NON, NON, NON, NON, TWO, TWO, NON },
-            { NON, NON, NON, NON, NON, NON, NON, NON, TWO, NON },
-            { TWO, TWO, NON, NON, TWO, NON, NON, TWO, NON, NON },
-            { NON, NON, NON, TWO, TWO, NON, NON, NON, NON, NON },
-            { NON, TWO, NON, NON, NON, NON, NON, NON, NON, NIL },
-            { NON, TWO, TWO, NON, NON, NON, NON, NON, NON, NON },
-            { NON, NON, TWO, NON, NON, NON, NON, NON, NON, NON },
-            { TWO, TWO, NON, NON, NON, TWO, NON, NON, NON, NON }
-    };
-
-    std::vector<std::vector<Piece >> new_assignment = {
-            { NON, NON, NON, NON, NON, NON, NON, NON, TWO, TWO },
-            { NON, TWO, NON, NON, NON, NON, NON, NON, NON, NIL },
-            { NON, NON, NON, NON, NON, NON, NON, NON, NON, NON },
-            { TWO, TWO, NON, NON, NON, NON, NON, NON, NON, NON },
-            { NON, TWO, TWO, NON, NON, NON, NON, NON, NON, NON },
-            { NON, TWO, NON, NON, NON, NON, NON, NON, NIL, NON },
-            { NON, NON, NON, NON, NON, NON, NON, NON, NON, NON },
-            { NON, NON, NON, NON, NON, NON, NON, NON, NON, NON },
-            { NON, NON, NON, NON, NON, NON, NON, NON, NON, NON },
-            { NON, NON, NON, NON, NON, NON, NON, NON, NON, NON }
-    };
-
-    std::vector<std::vector<Piece >> double_assignment = {
-            { NON, NON, NON, TWO, TWO, NON, NON, NON, TWO, TWO },
-            { NON, TWO, NON, NON, NON, NON, NON, NON, NON, NON },
-            { TWO, TWO, NON, NON, NON, NON, NON, NON, NON, NON },
-            { NON, TWO, TWO, NON, NON, NON, NON, NON, NON, NON },
-            { NON, TWO, NON, NIL, NON, NON, NON, NON, NON, NON },
-            { NON, NON, NON, NON, NON, NON, NON, NON, NON, NON },
-            { NON, NON, NON, NON, NON, NON, TWO, NON, NON, NIL },
-            { TWO, NON, NON, NON, NON, NON, TWO, NON, NON, NON },
-            { NON, NON, NON, NON, NON, NON, TWO, TWO, NON, NON },
-            { NON, NON, NON, NON, NON, NON, TWO, NON, NON, NON }
-    };
-
-    size = 11;
-    std::vector<std::vector<Piece >> and_gate = {
-            { NON, NON, NON, NIL, NIL, NON, NON, NON, NON, NON, NON },
-            { NON, NON, NON, NON, NON, NON, NON, NON, NON, TWO, NON },
-            { NON, NON, NON, NON, NIL, NON, TWO, NON, NON, TWO, NON },
-            { NON, NON, NON, NIL, TWO, NON, TWO, NON, NON, NON, NON },
-            { NON, NON, NON, NON, NON, NON, NON, NON, NON, NON, NON },
-            { NON, TWO, NON, NON, NON, NON, TWO, TWO, NON, NON, NON },
-            { NON, NON, NON, NON, NON, NON, NON, TWO, NON, NON, NON },
-            { NON, NON, TWO, NON, NON, NON, TWO, NON, TWO, NON, NON },
-            { NON, NON, NON, NON, NON, NON, NON, NON, TWO, NON, NON },
-            { TWO, NON, NON, NON, NON, TWO, TWO, NON, NON, NON, TWO },
-            { NON, NON, NON, NON, NON, NON, NON, NON, NON, NON, TWO }
-    };
-
-    size = 12;
+    int size = 12;
     std::vector<std::vector<Piece >> example = {
-            { NON, NON, NON, NIL, NON, NIL, NON, NON, NON, NON, NON, NON },
-            { NON, NON, NON, NON, NON, NON, NON, NON, NON, NON, TWO, NON },
-            { NON, NON, NON, NON, TWO, NON, NON, NON, NON, NON, NON, NON },
-            { NON, NON, NON, NON, TWO, TWO, NON, TWO, NON, NON, TWO, NON },
-            { NON, NON, NON, NIL, NON, TWO, NON, TWO, NON, NON, NON, NON },
-            { NON, NON, NON, NON, NON, NON, NON, NON, NON, NON, NON, NON },
-            { NON, TWO, NON, NON, NON, NON, NON, TWO, TWO, NON, NON, NON },
-            { NON, NON, NON, NON, NON, NON, NON, NON, TWO, NON, NON, NON },
-            { NON, NON, TWO, NON, NON, NON, NON, TWO, NON, TWO, NON, NON },
-            { NON, NON, NON, NON, NON, NON, NON, NON, NON, TWO, NON, NON },
-            { TWO, NON, NON, NON, NON, NON, TWO, TWO, NON, NON, NON, TWO },
-            { NON, NON, NON, NON, NON, NON, NON, NON, NON, NON, NON, TWO }
+            {	NON, 	NON, 	NON, 	NON, 	NON, 	NON, 	NON, 	NON, 	NON, 	NIL, 	NON, 	NIL 	},
+            {	NON, 	NON, 	NON, 	NON, 	TWO, 	NON, 	NON, 	NON, 	NON, 	NON, 	NON, 	NON 	},
+            {	NON, 	NON, 	NON, 	NON, 	NON, 	NON, 	NON, 	NON, 	NON, 	NON, 	TWO, 	NON 	},
+            {	NON, 	NON, 	NON, 	NON, 	TWO, 	TWO, 	NON, 	NON, 	NON, 	TWO, 	TWO, 	NON 	},
+            {	NON, 	NON, 	NON, 	NON, 	NON, 	TWO, 	NON, 	NON, 	NON, 	TWO, 	NON, 	NIL 	},
+            {	NON, 	NON, 	NON, 	NON, 	NON, 	NON, 	NON, 	NON, 	NON, 	NON, 	NON, 	NON 	},
+            {	NON, 	TWO, 	NON, 	NON, 	NON, 	TWO, 	TWO, 	NON, 	NON, 	NON, 	NON, 	NON 	},
+            {	NON, 	NON, 	NON, 	NON, 	NON, 	NON, 	TWO, 	NON, 	NON, 	NON, 	NON, 	NON 	},
+            {	NON, 	NON, 	TWO, 	NON, 	NON, 	TWO, 	NON, 	TWO, 	NON, 	NON, 	NON, 	NON 	},
+            {	NON, 	NON, 	NON, 	NON, 	NON, 	NON, 	NON, 	TWO, 	NON, 	NON, 	NON, 	NON 	},
+            {	TWO, 	NON, 	NON, 	NON, 	NON, 	TWO, 	NON, 	NON, 	TWO, 	NON, 	NON, 	NON 	},
+            {	NON, 	NON, 	NON, 	NON, 	NON, 	NON, 	NON, 	NON, 	NON, 	NON, 	NON, 	NON 	}
     };
 
     Position position(example);
-    Board board(position, size);
+    Board board(position, size), board2(full_and_12, 12);
+    board2.print();
     board.print();
 
     solve(board);
